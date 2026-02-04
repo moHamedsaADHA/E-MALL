0276a7d801b95f5c3bb71e7f15486c9734e9d578
moHamedsaADHA <msmsmsh222789@gmail.com>
2026-02-04 16:20:24 +0200

    Revert "Merge pull request #1 from Mo7ammedHassan/project_setup-and-auth"
    This reverts commit 8c3d75d7f6c2b370e9a69e199a6edbb74d73e0a1, reversing
    changes made to 77b862295229a8231e6f27da20da1dad673e2b1b.

--- src/utils/generate-token.ts
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken"
-const generateToken = (id: number,secretKey : string,JWT_EXPIRES:string): string => {
-
-  return jwt.sign({ id }, secretKey as string, {
-    expiresIn: JWT_EXPIRES as jwt.SignOptions["expiresIn"],
-  });
-};
-
-export default generateToken;
